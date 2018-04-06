9ce822842786a7057e40efcf8f3c548cb1c4ec14
norbert barics <norbert.barics@gmail.com>
2018-04-06 22:36:10 +0200

    refactor first task with java 8 stream

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,8 @@ public class Main {
   public static void main(String[] args) throws ParseException {
     long timeAtStart = System.currentTimeMillis();
 
-    Task03 task03 = new Task03();
-    System.out.println(task03.task03Solution());
+    Task01 task01 = new Task01();
+    System.out.println(task01.task01Solution());
 
     long runtime = System.currentTimeMillis() - timeAtStart;
     String oneMinuteRule = runtime <= 60000 ? "RUNTIME: " + runtime + " MILLIS": "ONE MINUTE PASSED, NEED TO REFACTOR";
--- src/Task01.java
+++ src/Task01.java
@@ -1,12 +1,8 @@
+import java.util.stream.IntStream;
 
 public class Task01 {
-  public int task01Solution(){
-    int solutionInt = 0;
-    for (int i = 0; i < 1000; ++i) {
-      if((i % 5 == 0) || (i % 3 == 0)) {
-        solutionInt += i;
-      }
+
+    public int task01Solution() {
+      return IntStream.range(0, 1000).filter(e -> e % 5 == 0|| e % 3 == 0).sum();
     }
-    return solutionInt;
-  }
 }
